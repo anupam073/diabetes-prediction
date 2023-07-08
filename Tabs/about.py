e286145ecc509051ff58cc412c9b30e913f21386
anupam073 <anupam.2003.mohanty@gmail.com>
2023-07-08 20:58:45 +0530

    DPS

---
+++ Tabs/about.py
@@ -0,0 +1,15 @@
+"""This modules contains data about about page"""
+
+# Import necessary modules
+import streamlit as st
+from PIL import Image
+
+
+def app():
+    """This function creates the about page"""
+    st.balloons()
+    st.title('Contact Us')
+    st.markdown('''### Name: Anupam Mohanty''')
+    st.markdown('''Passionate Engineer and Rational Thinker''')
+    st.markdown('''### Portfolio: [Anupam Mohanty](https://anupam-mohanty.netlify.app)''')
+    
